370218a0b6bc5a1a9a02a185bde4880d3097254b
cobalt77 <joekralicky@gmail.com>
2020-04-23 13:52:18 -0400

    Update cl_updatecheck.lua

--- CustomCommands/lua/autorun/client/cl_updatecheck.lua
+++ CustomCommands/lua/autorun/client/cl_updatecheck.lua
@@ -1,6 +1,6 @@
 local version = "4.16.19" -- m.d.yy
 http.Fetch("http://raw.githubusercontent.com/cobalt77/Custom-ULX-Commands/master/version.txt", function( body, len, headers, code)
-  local nversion = body
+  local nversion = body:Trim()
   if nversion != version then
     MsgC(Color(0,191,255), "[ULX-Custom-Commands]", color_white, ": A new version is available on GitHub.\n")
   end
